c8efd4ec5906fbcd0da503980529db395841485e
Rishabh <rishabh@Rishabhs-MacBook-Air.local>
2020-01-20 00:36:26 +0530

    Making mulithreaded

--- pythonProject/practice.py
+++ pythonProject/practice.py
@@ -1,16 +1,16 @@
 import requests
 import time
+import concurrent.futures
 
 def doSomething(delay):
 	time.sleep(delay)
-	for i in range (delay):
-		response = requests.get('http://localhost:8082/hello/rishabh')
-		if response.status_code == 200:
-		    print('Success!')
-		elif response.status_code == 404:
-		    print('Not Found.')
+	response = requests.get('http://localhost:8082/hello/rishabh')
+	if response.status_code == 200:
+		print('Success!')
+	elif response.status_code == 404:
+		print('Not Found.')
 
+for delay in range(6):
+	with concurrent.futures.ThreadPoolExecutor() as executor:
+		results = [executor.submit(doSomething,delay) for _ in range(delay)]
 
-if __name__ == "__main__":
-	for i in range(10):
-		doSomething(i)
